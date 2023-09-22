4d4fbc1cc2101ea200d3bf89a39c4e024c723559
manumujica <145512473+manumujica@users.noreply.github.com>
2023-09-22 17:27:45 -0300

    Add files via upload

---
+++ tpe_fonoteca.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-09-2023 a las 22:14:00
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tpe_fonoteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas`
+--
+
+CREATE TABLE `artistas` (
+  `id_artista` int(11) NOT NULL,
+  `artist_name` varchar(100) NOT NULL,
+  `artist_dob` date NOT NULL,
+  `artist_pob` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `canciones`
+--
+
+CREATE TABLE `canciones` (
+  `id_song` int(11) NOT NULL,
+  `song_name` varchar(100) NOT NULL,
+  `length` time NOT NULL,
+  `id_album` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `discos`
+--
+
+CREATE TABLE `discos` (
+  `id_album` int(11) NOT NULL,
+  `album_name` varchar(100) NOT NULL,
+  `release_date` date NOT NULL,
+  `id_genre` int(11) NOT NULL,
+  `id_artist` int(11) NOT NULL,
+  `length` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generos`
+--
+
+CREATE TABLE `generos` (
+  `id_genre` int(11) NOT NULL,
+  `genre_name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  ADD PRIMARY KEY (`id_artista`);
+
+--
+-- Indices de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD PRIMARY KEY (`id_song`),
+  ADD KEY `idx_id_album` (`id_album`);
+
+--
+-- Indices de la tabla `discos`
+--
+ALTER TABLE `discos`
+  ADD PRIMARY KEY (`id_album`) USING BTREE,
+  ADD KEY `idx_id_genre` (`id_genre`),
+  ADD KEY `idx_id_artist` (`id_artist`);
+
+--
+-- Indices de la tabla `generos`
+--
+ALTER TABLE `generos`
+  ADD PRIMARY KEY (`id_genre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  MODIFY `id_artista` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  MODIFY `id_song` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `discos`
+--
+ALTER TABLE `discos`
+  MODIFY `id_album` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `generos`
+--
+ALTER TABLE `generos`
+  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `discos`
+--
+ALTER TABLE `discos`
+  ADD CONSTRAINT `discos_ibfk_1` FOREIGN KEY (`id_genre`) REFERENCES `generos` (`id_genre`),
+  ADD CONSTRAINT `discos_ibfk_2` FOREIGN KEY (`id_album`) REFERENCES `canciones` (`id_album`),
+  ADD CONSTRAINT `discos_ibfk_3` FOREIGN KEY (`id_artist`) REFERENCES `artistas` (`id_artista`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
