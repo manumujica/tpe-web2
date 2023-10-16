3fb4d4a11585e0d2cb22081da9373c94711707b8
Manuel Mujica <manuelmujica1973@gmail.com>
2023-10-16 18:32:39 -0300

    comienza implementación MVC

--- app/models/discos.model.php
+++ app/models/discos.model.php
@@ -2,4 +2,16 @@
 
 class DiscosModel {
     
+    protected $db;
+
+    public function __construct() {
+        $this->db = DBHelper::getConection();
+    }
+
+    public function getAlbums(){
+        $query = $this->db->prepare('SELECT discos.*, artistas.artist_name as artist_name FROM discos JOIN artistas ON discos.id_artist = artistas.id_artista');
+        $query->execute();
+        $albums = $query->fetchAll(PDO::FETCH_OBJ);
+        return $albums;
+    }
 }
