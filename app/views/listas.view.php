8065aa4b3633a1b9bc2f36d51165c43a8fbafbc8
axelbustamante00 <vascoguitar@hotmail.com>
2023-10-17 11:59:10 -0300

    Trabajo hecho en funciones públicas. Estructura MVC, mostrar y filtrar listas

--- app/views/listas.view.php
+++ app/views/listas.view.php
@@ -1,9 +1,14 @@
 <?php
 
-class ListaDiscosView {
+class ListasView {
     public function showDiscos($discos) {
         $count = count($discos);
         require 'templates/discosList.phtml';
+    }
+
+    public function showArtistas($artistas) {
+        $count = count($artistas);
+        require 'templates/artistasList.phtml';
     }
 
     public function showError($error) {
--- router.php
+++ router.php
@@ -1,10 +1,10 @@
 <?php
-require_once './app/controllers/lista.discos.controller.php';
+require_once './app/controllers/listas.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
 
-$action = 'listar';
+$action = 'discos';
 
 if (!empty( $_GET['action'])) {
     $action = $_GET['action'];
@@ -15,8 +15,19 @@ if (!empty( $_GET['action'])) {
 $params = explode('/', $action);
 
 switch ($params[0]) {
-    case 'listar':
-        $controller = new ListaDiscosController();
+    case 'discos':
+        $controller = new ListasController();
         $controller->showDiscos();
+        break;
+    case 'artistas':
+        $controller = new ListasController();
+        $controller->showArtistas();
+        break;
+    case 'filtrar':
+        $controller = new ListasController();
+        $controller->filtrarDiscos();
+        break;
+    case 'login':
+        require 'templates/header.phtml';
         break;
     }
